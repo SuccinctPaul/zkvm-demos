c76cb229e0eb00cb534124a23e240181dabc10a4
Paul Cheng <chengyuejia@foxmail.com>
2025-11-05 22:53:54 +0800

    chore: add profiler for nexus

--- nexus-zkvm/nexus-guest/src/main.rs
+++ nexus-zkvm/nexus-guest/src/main.rs
@@ -1,9 +1,10 @@
 #![cfg_attr(target_arch = "riscv32", no_std, no_main)]
 
-use nexus_rt::println;
+use nexus_rt::{println, profile};
 
 #[nexus_rt::main]
 #[nexus_rt::public_input(x)]
+#[profile]
 fn main(x: u32) {
     println!("Read public input:  {}", x);
     let res = fib::fibonacci(x);
