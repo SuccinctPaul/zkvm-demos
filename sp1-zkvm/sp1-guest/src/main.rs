c0ed9b480fbdb26f505b841d659b0b01e6bc41d9
Paul Cheng <chengyuejia@foxmail.com>
2025-11-20 13:09:50 +0800

    feat: support multi program for sp1 zkvm instead use multi elf file

--- sp1-zkvm/sp1-guest/src/main.rs
+++ sp1-zkvm/sp1-guest/src/main.rs
@@ -6,52 +6,15 @@
 #![no_main]
 sp1_zkvm::entrypoint!(main);
 
-use common::{fibonacci, sum, factorial, is_prime, popcount, hash_sha256, verify_signature};
+use common::execute_program;
 
 pub fn main() {
     // Read program ID (0-6) and input parameter
     let program_id = sp1_zkvm::io::read::<u32>();
     let n = sp1_zkvm::io::read::<u32>();
 
-    println!("SP1 Guest: program_id={}, n={}", program_id, n);
-
-    // Execute the selected program
-    let result = match program_id {
-        0 => {
-            println!("Running Fibonacci({})", n);
-            fibonacci(n)
-        }
-        1 => {
-            println!("Running Sum({})", n);
-            sum(n)
-        }
-        2 => {
-            println!("Running Factorial({})", n);
-            factorial(n)
-        }
-        3 => {
-            println!("Running IsPrime({})", n);
-            is_prime(n)
-        }
-        4 => {
-            println!("Running PopCount({})", n);
-            popcount(n)
-        }
-        5 => {
-            println!("Running Hash({})", n);
-            hash_sha256(n)
-        }
-        6 => {
-            println!("Running Signature({})", n);
-            verify_signature(n)
-        }
-        _ => {
-            println!("Unknown program_id: {}, defaulting to Fibonacci", program_id);
-            fibonacci(n)
-        }
-    };
-
-    println!("Result: {}", result);
+    // Execute the selected program using common dispatcher
+    let result = execute_program(program_id, n);
 
     // Commit the result as public output
     sp1_zkvm::io::commit(&result);
